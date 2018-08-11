413fe3ba6aa09f0cae02129722df6e93c00465e2
Shine <xuxiang19930924@126.com>
2018-08-12 02:24:59 +0800

    init

---
+++ mlpy/supervised/perceptron.py
@@ -0,0 +1,6 @@
+# Copyright @2018 xuxiang. All rights reserved.
+#
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+import numpy as np
---
+++ mlpy/utils/activation_functions.py
@@ -0,0 +1,74 @@
+# Copyright @2018 xuxiang. All rights reserved.
+#
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, division
+import numpy as np
+
+class _Activation_Function_():
+    def print_name(self):
+        print(self.__class__.__name__)
+
+
+class Sigmoid(_Activation_Function_):
+    def __call__(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def gradient(self, x): # y*(1-y)
+        return self.__call__(x) * (1 - self.__call__(x))
+
+
+class Softmax(_Activation_Function_):
+    def __call__(self, x):
+        e_x = np.exp(x - np.min(x, axis=-1, keepdims=True)) # scale down
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+    
+    def gradient(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
+
+
+class Tanh(_Activation_Function_):
+    def __call__(self, x):
+        return 2 / (1 + np.exp(-2*x)) - 1
+    
+    def gradient(self, x): # 1-y**2
+        return 1 - np.power(self.__call__(x), 2)
+
+
+class ReLU(_Activation_Function_):
+    def __call__(self, x):
+        return np.where(x >= 0, x, 0)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, 0)
+
+
+class LeakyReLU(_Activation_Function_):
+    def __init__(self, alpha=0.01):
+        self.alpha = alpha
+
+    def __call__(self, x):
+        return np.where(x >= 0, x, self.alpha * x)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, self.alpha)
+
+
+class ELU(_Activation_Function_):
+    def __init__(self, alpha=0.01):
+        self.alpha = alpha
+
+    def __call__(self, x):
+        return np.where(x >= 0, x, self.alpha * (np.exp(x) - 1))
+
+    def gradient(self, x):
+        return np.where(x >= 0, x, self.alpha * np.exp(x))
+
+
+class Gaussian(_Activation_Function_):
+    def __call__(self, x):
+        return np.exp(-1 * np.power(x, 2))
+
+    def gradient(self, x):
+        return -2 * x * self.__call__(x)
+
